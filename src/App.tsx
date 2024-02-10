8d9ac8f197e27153c61b5dc569c48f4cfc6863cc
JuliaJPereira <juliapereira.odc@gmail.com>
2024-02-10 13:18:12 -0300

    Ordenar/Organizar mês/ano

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import Investments from './components/Investments';
 import data from './data.json';
+import { InvestmentsReport } from './Types';
+
+const reports = (data.reports as InvestmentsReport[]).sort((a, b) => {
+  if (a.month > b.month) return 1;
+  if (a.month < b.month) return -1;
+  return 0;
+});
 
 function App() {
-  return <Investments investments={data.investments} reports={data.reports} />;
+  return <Investments investments={data.investments} reports={reports} />;
 }
 
 export default App;
